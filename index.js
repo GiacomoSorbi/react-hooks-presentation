cb0217be404251a3bbe947cb63a3ee3eac9a9924
Giacomo Sorbi <giacomosorbi@Ajnaputer-2.local>
2019-01-09 19:19:28 +0000

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import Presentation from './example/src';
+import Presentation from './react-hooks-presentation/src';
 
 render(<Presentation />, document.getElementById('root'));
